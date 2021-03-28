c889e9a52b61ef1478c232ac7dec7b500c371318
hackerESQ <corey@coreyvarma.com>
2021-03-28 00:23:55 -0500

    clean up var names

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ module.exports = function resolveClientEnv (options) {
 
     // set default options
     const defaults = {
-        env_path: '../.env', 
-        prefixRE: /^VUE_APP_/,
+        path: '../.env', 
+        prefix: /^VUE_APP_/, // MIX_ or VITE_APP_ etc
         expand: true,
-        base_url:'/',
-        loadAdditionalKeys:[]
+        baseUrl: '/',
+        loadAdditionalKeys: []
     };
     options = { ...defaults, ...options };
 
     // get laravel env file
     const clientEnv = new dotenv.config({
-        path: path.join(process.cwd(), options.env_path)
+        path: path.join(process.cwd(), options.path)
     });
 
     // should we expand?
@@ -28,13 +28,13 @@ module.exports = function resolveClientEnv (options) {
     const env = {}
     Object.keys(clientEnv.parsed).forEach(key => {
         // only fetches ENVs that meet prefix requirement OR specifically requested
-        if (options.prefixRE.test(key) || key === 'NODE_ENV' || options.loadAdditionalKeys.includes(key)) {
+        if (options.prefix.test(key) || key === 'NODE_ENV' || options.loadAdditionalKeys.includes(key)) {
             env[key] = clientEnv.parsed[key]
         }
     })
 
     // sets the required vue variables
-    env.BASE_URL = options.base_url;
+    env.BASE_URL = options.baseUrl;
     env.NODE_ENV = process.env.NODE_ENV;
 
     // stringify keys so they're safe
